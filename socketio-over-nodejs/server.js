9c4bedd9bc61ed8d9a9b08920a3f9cf55f107b1c
shadow9503 <33388081+shadow9503@users.noreply.github.com>
2020-11-24 15:33:47 +0900

    11_24_uploadAWS
    11_24

---
+++ socketio-over-nodejs/server.js
@@ -0,0 +1,106 @@
+// https://www.webrtc-experiment.com/
+
+var fs = require('fs');
+
+// SSL 인증서 (임시)
+var options = {
+    key: fs.readFileSync('fake-keys/privatekey.pem'),
+    cert: fs.readFileSync('fake-keys/certificate.pem')
+};
+
+// HTTPs server
+var app = require('https').createServer(options, function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    response.end();
+});
+
+
+// socket.io goes below
+var io = require('socket.io').listen(app, {
+    log: true,
+    // origins: '*:*'
+});
+
+io.set('transports', [
+    // 'websocket',
+    'xhr-polling',
+    'jsonp-polling'
+]);
+
+var channels = {};
+var broadcasterid = null;
+/* 
+    TODO: 
+        * admin소켓 저장하여야함. 리스너 만들것
+        * 캐티에서 응답받은 response를 tomcat으로 post해야함.
+*/
+
+// io.sockets.on 내부에서 socket 핸들링할것.
+io.sockets.on('connection', function (socket) {
+    var initiatorChannel = '';
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+
+    socket.on('new-channel', function (data) {
+        if (!channels[data.channel]) {
+            initiatorChannel = data.channel;
+        }
+
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
+
+    
+
+});
+
+// 채널명으로 namespace 관리
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        var username;
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+        
+        socket.on('message', function (data) {
+            if (data.sender == sender) {
+                if(!username) username = data.data.sender;
+                if(!broadcasterid) broadcasterid = data.data.broadcaster;
+                socket.broadcast.emit('message', data.data);
+            }
+        });
+        
+        socket.on('disconnect', function() {
+            if(username) {
+                socket.broadcast.emit('user-left', username);
+                username = null;
+            }
+        });
+    });
+}
+
+// run app
+
+app.listen(9559);
+
+process.on('unhandledRejection', (reason, promise) => {
+  process.exit(1);
+});
+
+// console.log('Please open SSL URL: https://192.168.219.172:'+(9559)+'/');
+console.log('Please open SSL URL: https://172.16.200.33:'+(9559)+'/');
